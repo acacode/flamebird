a6734e6ace6e8adca0b1c94b46190c655b194be6
Volkov Sergey <truefunrun@hotmail.com>
2018-04-20 05:40:16 +0300

    add web-view

---
+++ README.md
@@ -0,0 +1,14 @@
+# Flamebird.js
+  
+Wonderful task runner, works in the command line and also have own webview.
+
+## Installation
+
+    $ npm install -g flamebird
+
+## How it works
+ 
+  After installing `flamebird`, open command line in your project folder<br>
+  and write `flamebird`. After that, flamebird takes your `Procfile` or if<br>
+  you don't have Procfile then flamebird takes your `package.json` and <br>
+  by default will be use `start` script
--- flamebird.js
+++ flamebird.js
@@ -4,49 +4,47 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-var program = require('commander')
-var packageJson = require('./package.json')
+const program = require('commander')
+const utils = require('./lib/utils')
 const processWorker = require('./lib/processWorker')(process)
 
-program.version(getLogoWithVersion(true), '-v, --version')
+program.version(utils.getLogoWithVersion(), '-v, --version')
 program.option('-j, --procfile <FILE>', 'load procfile from file', 'Procfile')
 program.option(
   '-e, --env <FILE>',
   'load environment from file, a comma-separated list',
   '.env'
 )
-program.option('-p, --port <PORT>', 'start indexing ports at number PORT', 0)
+program.option('-P, --port <PORT>', 'start indexing ports at number PORT', 0)
 program
   .command('start')
   .usage('[Options]')
-  .description('Start the jobs in the Procfile')
-  .action(function(args) {                                                             
-    const procfile = require('./lib/procfile').load(program.procfile)
-    processWorker.start(procfile, program.port || process.env.PORT || 5050)
+  .option(
+    '-w, --web',
+    'Launch webview for managing jobs. If this flag is setted then tasks will not be running in the command line'
+  )
+  .option('-p, --package', 'Use package.json for managing tasks')
+  .option(
+    '-t, --tasks [tasks]',
+    'List of tasks which will be run flamebird ( example : --tasks start,start:dev,start-server )'
+  )
+  .description('Start the jobs in the Procfile/Package.json')
+  .action(function(args) {
+    const taskfile = require('./lib/taskfile').load(program.procfile, args)
+    if (args.web) {
+      processWorker.web(
+        taskfile,
+        program.port || process.env.PORT || 5050,
+        args
+      )
+    } else {
+      processWorker.start(taskfile, args)
+    }
   })
 
 program.parse(process.argv)
 
-function getLogoWithVersion(onlyVersion){
-  var strings = []
-  if(!onlyVersion){
-     strings.push('  ╔══╗ ╔╗   ╔══╗ ╔╗  ╔╗ ╔═══╗ ╔══╗  ╔══╗ ╔═══╗ ╔══╗ ')
-     strings.push('  ║╔═╝ ║║   ║╔╗║ ║║  ║║ ║╔══╝ ║╔╗║  ╚╗╔╝ ║╔═╗║ ║╔╗╚╗')
-     strings.push('  ║╚═╗ ║║   ║╚╝║ ║╚╗╔╝║ ║╚══╗ ║╚╝╚╗  ║║  ║╚═╝║ ║║╚╗║')
-     strings.push('  ║╔═╝ ║║   ║╔╗║ ║╔╗╔╗║ ║╔══╝ ║╔═╗║  ║║  ║╔╗╔╝ ║║ ║║')
-     strings.push('  ║║   ║╚═╗ ║║║║ ║║╚╝║║ ║╚══╗ ║╚═╝║ ╔╝╚╗ ║║║║  ║╚═╝║')
-     strings.push('  ╚╝   ╚══╝ ╚╝╚╝ ╚╝  ╚╝ ╚═══╝ ╚═══╝ ╚══╝ ╚╝╚╝  ╚═══╝')
-     strings.push('             - wonderful nodejs task manager        ')
-  }
-  const v = packageJson.version + (new Array(11 - packageJson.version.length).join(' '))
-  const commonSpace = onlyVersion ? '  ' : '                           '
-   strings.push(commonSpace+'╔═══════════════╗  ')
-   strings.push(commonSpace+'║    v' + v + '║  ')
-   strings.push(commonSpace+'╚═══════════════╝  ')
-   return strings.join('\r\n')
-}
 if (!process.argv.slice(2).length) {
-  console.log(getLogoWithVersion())
-  process.argv.push('start')
-  program.parse(process.argv)
+  console.log(utils.getLogoWithVersion())
+  program.outputHelp()
 }
