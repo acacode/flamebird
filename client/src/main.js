fe54a7b824a6e9e57b10eef579b2cc7934100d20
Volkov Sergey <truefunrun@hotmail.com>
2019-07-02 01:24:25 +0300

    feat: change file format of svelte components (.svelte -> .html); feat: fix problems with Y position of tabs

--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,17 @@
-import App from "./components/App.svelte"
+import App from "./components/App.html"
 import store from './store'
 
 
 const initializeApp = async () => {
 	const appInfo = await store.modules.app.getInfo()
 	store.modules.tasks.createTasks(appInfo.commands)
-	return new App({
+	const app = new App({
 		target: document.body,
 		props: {
 			name: "world"
 		}
 	})
+	return app
 }
 
 export default initializeApp()
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,11 @@
-import {StonexStore} from 'stonex'
+import { StonexStore } from 'stonex'
 import Tabs from './modules/Tabs'
 import Tasks from './modules/Tasks'
 import App from './modules/App'
 
+
+// TODO: add middleware which will update svelte application
+// https://github.com/lukechinworth/codenames/blob/svelte/src/index.js
 const store = new StonexStore({
 	app: App,
 	tabs: Tabs,
